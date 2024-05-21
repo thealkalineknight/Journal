cd77ff19b3057de13cd49cbc8c92680340226671
thecringefactor <139799163+thecringefactor@users.noreply.github.com>
2024-05-21 13:18:32 -0400

    Create wantedPoster.js

---
+++ 2021/wantedPoster.js
@@ -0,0 +1,39 @@
+background(209, 207, 207);
+line(0, 37, 10000, 26);
+line(0, 80, 10000, 26);
+line(0, 146, 10000, 26);
+line(0, 228, 10000, 26);
+line(0, 291, 10000, 26);
+line(0, 350, 10000, 26);
+line(0, 10000, 127, -53);
+line(0, 2900, 269, -53);
+line(0, 10000, 375, -53);
+
+noStroke();
+fill(229, 232, 127);
+rect(50, 10, 300, 350);
+fill(240, 238, 178);
+rect(70, 30, 260, 60);
+rect(70, 118, 260, 200);
+
+fill(206, 209, 170);
+ellipse(170, 230, 130, 130);
+fill(73, 74, 66);
+ellipse(204, 205, 20, 20);
+ellipse(148, 218, 20, 20);
+fill(125, 125, 125);
+ellipse(186, 250, 55, 68);
+
+fill(1, 1, 1);
+textSize(40);
+text("WANTED", 110, 80);
+textSize(20);
+text("WINSTON", 221, 143);
+textSize(10);
+text("(That One Cool Coding Buddy)", 186, 156);
+textSize(15);
+text("For being cool.", 225, 177);
+textSize(20);
+text("Reward:", 228, 289);
+textSize(15);
+text("Fun learning and fantastic knowledge!", 74, 311);
