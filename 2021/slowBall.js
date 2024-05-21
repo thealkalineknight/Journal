022546fa2f2f6667a8bf4b0db047ba976f2ad2a6
thecringefactor <139799163+thecringefactor@users.noreply.github.com>
2024-05-21 13:20:04 -0400

    Create slowBall.js

---
+++ 2021/slowBall.js
@@ -0,0 +1,22 @@
+// position of the ball
+var y = 0;
+// how far the ball moves every time
+var speed = 2;
+
+draw = function() {
+    background(127, 204, 255);
+    
+    fill(66, 66, 66);
+    ellipse(200, y, 50, 50);
+
+    // move the ball
+    y = y + speed;
+    
+    if (y > 400) {
+        speed = -2;
+        
+    }
+    if (y < 0) {
+        speed = 2;
+    }
+};
