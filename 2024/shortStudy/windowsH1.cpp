ebac59d87625f34c53e83b7dee80a4e21e5ef104
thecringefactor <139799163+thecringefactor@users.noreply.github.com>
2024-05-21 15:43:19 -0400

    Create windowsH1.cpp

---
+++ 2024/shortStudy/windowsH1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <Windows.h>
+#include <cctype>
+using namespace std;
+
+
+int main() {  // nonsensical output for lulz
+	while (true) {
+		for (int i = 0; i < 256; i++) {
+			if (GetAsyncKeyState('A') & 0b1) {  //&0b1
+				if (std::isupper(i)) {
+					cout << (char)i;
+				}
+				else if (i == VK_ESCAPE) {
+					cout << "esc";
+				}
+				// etc
+			}
+		}
+	}
+	std::cin.get();
+	return 0;
+}
