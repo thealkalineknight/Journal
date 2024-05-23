5b43b2d9f3e8b48e0f83a5af80612c02526d1156
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-05-23 14:35:24 -0400

    Create insertionSort.cpp

---
+++ 2024/shortStudy/insertionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertionSort(vector<int>&nums) {
+	for (int i = 1; i < nums.size(); i++) {
+		int current = nums[i];
+		int revJ = i - 1;
+		while (revJ >= 0 && nums[revJ] > current) {
+			nums[revJ + 1] = nums[revJ];
+			revJ -= 1;
+		}
+		nums[revJ + 1] = current;
+	}
+	for (int i = 0; i < nums.size(); i++) {
+		cout << nums[i] << " ";
+	}
+}
+
+int main() {
+	int count{};
+	cout << "input num count" << '\n';
+	cin >> count;
+
+	vector<int> input{};
+	cout << "input nums" << '\n';
+	for (int i = 0; i < count; i++) {
+		int num{};
+		cin >> num;
+		input.push_back(num);
+	}
+
+	insertionSort(input);
+
+	return 0;
+}
