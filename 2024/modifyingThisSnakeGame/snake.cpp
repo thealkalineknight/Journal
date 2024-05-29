054bba317882f5449168e471296a6bfaf4b85efa
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-05-29 14:41:03 -0400

    Update snake.cpp

--- 2024/modifyingThisSnakeGame/snake.cpp
+++ 2024/modifyingThisSnakeGame/snake.cpp
@@ -1,15 +1,37 @@
 #include "snake.h"
 
 Snake::Snake() {
-    deque<Vector2> body = { Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9} };
     Vector2 direction = { 1, 0 };
     bool addSegment = false;
 }
 
 Snake::~Snake() {
 }
+// https://github.com/educ8s/Cpp-Retro-Snake-Game-with-raylib/blob/main/main.cpp
+void Snake::Update() {
+    Move();
+    Draw();  // evemt of death?
+}
 
 void Snake::Draw() {
+    for (int i = 0; i < body.size(); i++) {
+        int x = body[i].x;
+        int y = body[i].y;
+        DrawRectangle(x * st.cellSize, y * st.cellSize, st.cellSize, st.cellSize, st.aColor);
+    }
+}
+
+void Snake::Move() {
+    if (IsKeyPressed(KEY_UP) && direction.y != 1) {
+       direction = { 0, -1 };
+    }
+    if (IsKeyPressed(KEY_DOWN) && direction.y != -1) {
+        direction = { 0, 1 };
+    }
+    if (IsKeyPressed(KEY_LEFT) && direction.x != 1) {
+        direction = { -1, 0 };
+    }
+    // if (IsKeyPressed(KEY_RIGHT) && direction.x != -1)
 }
 
 void Snake::Death() {
