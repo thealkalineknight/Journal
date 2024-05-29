faf681e4c70d4ba5fefa79b58e435cfc9c4d4891
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-05-29 14:41:16 -0400

    Update snake.h

--- 2024/modifyingThisSnakeGame/snake.h
+++ 2024/modifyingThisSnakeGame/snake.h
@@ -7,14 +7,16 @@ public:
 	Settings st;
 	Vector2 position;
 
-	deque<Vector2> body;
+	deque<Vector2> body = { Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9} };
 	Vector2 direction;
 	bool addSegment;
 
 	Snake();
 	~Snake();
 
+	void Update();
 	void Draw();
+	void Move();
 	void Death();
 
 private:
