a41d9a413cb0b23e35802d2d7a88c2b04918b330
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-05-30 13:17:21 -0400

    Update snake.h

--- 2024/modifyingThisSnakeGame/snake.h
+++ 2024/modifyingThisSnakeGame/snake.h
@@ -8,9 +8,9 @@ public:
 	Vector2 position;
 
 	deque<Vector2> body = { Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9} };
-	Vector2 direction;
+	Vector2 direction = { 1, 0 };
+
 	bool addSegment;
-	bool dirSwitch = true;
 
 	Snake();
 	~Snake();
