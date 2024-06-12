2f31e35d94640a6514aae4296c5c955b7b6a7173
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-06-12 19:54:44 -0400

    Update cube.py

--- 2024/3DmatricesLite/cube.py
+++ 2024/3DmatricesLite/cube.py
@@ -25,11 +25,20 @@ class Cube:
                         [1, -1, -5],
                         [1, 1, -5],
                         [-1, 1, -5]]
+        self.matrixAt = [[0, 0, 0],
+                         [0, 0, 0],
+                         [0, 0, 0],
+                         [0, 0, 0],
+                         [0, 0, 0],
+                         [0, 0, 0],
+                         [0, 0, 0],
+                         [0, 0, 0]]
 
     def update(self):
         v = 0
 
     def draw(self):
+        at = self.matrixAt
         for i in range(len(self.matrixA)):
             x = self.matrixA[i][0] / -self.matrixA[i][2]
             y = self.matrixA[i][1] / -self.matrixA[i][2]
@@ -38,6 +47,14 @@ class Cube:
 
             pg.draw.circle(self.game.screen, 'green',
                            (x, y), 5)
+            at[i][0] = x
+            at[i][1] = y
+            if i + 1 < len(at):
+                pg.draw.line(self.game.screen, 'white', (at[i][0], at[i][1]), (at[i+1][0], at[i+1][1]))
+            if i < 3:
+                pg.draw.line(self.game.screen, 'white', (at[i][0], at[i][1]), (at[7-i][0], at[7-i][1]))
+        pg.draw.line(self.game.screen, 'white', (at[0][0], at[0][1]), (at[3][0], at[3][1]))
+        pg.draw.line(self.game.screen, 'white', (at[4][0], at[4][1]), (at[7][0], at[7][1]))
 
     def remap(self, point, p):
         point = (1 + point) / 2
@@ -57,4 +74,3 @@ class Cube:
             self.matrixA[i][0] = x * ms[0][0] + y * ms[1][0] + z * ms[2][0]
             self.matrixA[i][1] = x * ms[0][1] + y * ms[1][1] + z * ms[2][1]
             self.matrixA[i][2] = x * ms[0][2] + y * ms[1][2] + z * ms[2][2]
-
