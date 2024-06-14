36a0dedacb0f3b21868eb9470bfc3e9206b2fdbc
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-06-14 19:47:34 -0400

    Update sstreamLogicalThinking.cpp

--- 2024/shortStudy/sstreamLogicalThinking.cpp
+++ 2024/shortStudy/sstreamLogicalThinking.cpp
@@ -31,7 +31,7 @@ int main() {
 /*
 Fig1: 1. Must use vec cause func name, and non-fixed len. 2. But unkown len usually calls for a loop. 3. Can ss take vecs?
 vector<int> parseInts(string str) {
-	  stringstream ss(str);
+    stringstream ss(str);
     char ch;
     vector<int> nums;
     ss >> nums >> ch;
@@ -40,7 +40,7 @@ vector<int> parseInts(string str) {
 
 Fig2: 1. No, ss takes int. 2. Won't this only return 1 number?
 vector<int> parseInts(string str) {
-	  stringstream ss(str);
+    stringstream ss(str);
     char ch;
     vector<int> nums;
     ss >> a >> ch;
@@ -50,7 +50,7 @@ vector<int> parseInts(string str) {
 
 Fig3: 1. Yes, so use a loop. 2. While something exists. 3. While a exists in str.
 vector<int> parseInts(string str) {
-	  stringstream ss(str);
+    stringstream ss(str);
     char ch;
     vector<int> nums;
     while (ss >> a) {  // does action in here while compare
