86018eb904dc4e06c416dcef9985b9fe61a84a0f
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2025-01-07 17:34:18 -0500

    Create hello.cbl

---
+++ 2025/cbl/hello.cbl
@@ -0,0 +1,11 @@
+      $set dialect(ANS85)
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOWLD.
+
+       PROCEDURE DIVISION.
+
+       100-PROGRAM.
+           DISPLAY 'HELLO AAA'
+           STOP RUN
+           .
+           
